5cc9516579da00782fbb1fab4b915dc1b7ee14d5
HassanKhan123 <hk260280@gmail.com>
2026-01-20 22:49:43 +0100

    solved: reverse string problem

---
+++ 01-basic-challenges-1/reverse-string/index.js
@@ -0,0 +1,12 @@
+const reverseString = (str) => {
+  let revStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    revStr += str[i];
+  }
+
+  return revStr;
+};
+
+const res = reverseString("Hassan");
+console.log(res);

4950b9f9844f27f313c55ede955693394f7866e7
HassanKhan123 <hk260280@gmail.com>
2026-01-21 15:32:39 +0100

    solved: fizzbuzz problem

---
+++ 02-basic-challenges-2/fizz-buzz-array/index.js
@@ -0,0 +1,20 @@
+const fizzBuzzArray = (num) => {
+  const results = [];
+
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      results.push("FizzBuzz");
+    } else if (i % 3 == 0) {
+      results.push("Fizz");
+    } else if (i % 5 == 0) {
+      results.push("Buzz");
+    } else {
+      results.push(i);
+    }
+  }
+
+  return results;
+};
+
+const res = fizzBuzzArray(15);
+console.log(res);

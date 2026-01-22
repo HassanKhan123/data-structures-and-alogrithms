bade47496814ed5d8e8e2a068045c722d77976f6
HassanKhan123 <hk260280@gmail.com>
2026-01-22 18:59:30 +0100

    solved: hashtag generator problem

--- 03-higher-order-array-methods/hashtag-generator/index.js
+++ 03-higher-order-array-methods/hashtag-generator/index.js
@@ -1,12 +1,15 @@
 const hashtagGenerator = (str) => {
   if (str.trim() === "") return false;
 
-  const hashtag = str.split(" ").reduce((tag, word) => {
-    return tag + word[0].toUpperCase() + word.substring(1).toLowerCase();
-  }, "#");
+  const hashtag = str
+    .trim()
+    .split(" ")
+    .reduce((tag, word) => {
+      return tag + word[0].toUpperCase() + word.substring(1).toLowerCase();
+    }, "#");
 
   return hashtag.length > 140 ? false : hashtag;
 };
 
-const res = hashtagGenerator("javascript is amazing");
+const res = hashtagGenerator(" javascript is amazing");
 console.log(res);

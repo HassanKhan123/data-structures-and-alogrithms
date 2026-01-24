f1007eae8bd1f7b4a09e93051e12534dfbec7601
HassanKhan123 <hk260280@gmail.com>
2026-01-24 23:37:58 +0100

    solved: permutations problem

---
+++ 04-recursion/permutations/index.js
@@ -0,0 +1,27 @@
+const permutations = (str) => {
+  let result = [];
+
+  if (str.length === 0) {
+    result.push("");
+    return result;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const firstChar = str[i];
+    const restOfString = str.slice(0, i) + str.slice(i + 1);
+    const subPermutations = permutations(restOfString);
+
+    console.log("subPermutations", subPermutations);
+
+    console.log(firstChar, restOfString);
+
+    for (let j = 0; j < subPermutations.length; j++) {
+      result.push(firstChar + subPermutations[j]);
+    }
+  }
+
+  return result;
+};
+
+const res = permutations("abc");
+console.log(res);

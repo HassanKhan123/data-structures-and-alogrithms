f0eac67b0d42e43cf70ef81fc1c1b9cf65abe2b7
HassanKhan123 <hk260280@gmail.com>
2026-02-09 19:01:10 +0100

    solved: palindrome queue stack problem

---
+++ 07-stacks-queue-linked-lists/palindrome-queue-stack/index.js
@@ -0,0 +1,26 @@
+import { Queue } from "../queue.js";
+import { Stack } from "../stack.js";
+
+const isPalindromeQueueStack = (str) => {
+  const formattedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  const charQueue = new Queue();
+  const charStack = new Stack();
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    const char = formattedStr.charAt(i);
+    charQueue.enqueue(char);
+    charStack.push(char);
+  }
+
+  while (!charQueue.isEmpty()) {
+    if (charQueue.dequeue() === charStack.pop()) {
+      return true;
+    }
+
+    return false;
+  }
+};
+
+const res = isPalindromeQueueStack("racecar");
+console.log(res);

54123ad74d74a9a8458f1ecc4f05259e2e4615a1
HassanKhan123 <hk260280@gmail.com>
2026-02-11 19:12:17 +0100

    added: depthFirstTraversal

---
+++ 08-binary-trees-graphs/depth-first-traversal/depth-first-traversal.js
@@ -0,0 +1,48 @@
+export class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function depthFirstTraversal(root) {
+  if (!root) return [];
+
+  const result = [];
+  const stack = [];
+
+  stack.push(root);
+
+  while (stack.length > 0) {
+    const current = stack.pop();
+
+    result.push(current.data);
+
+    if (current.right) {
+      stack.push(current.right);
+    }
+
+    if (current.left) {
+      stack.push(current.left);
+    }
+  }
+
+  return result;
+}
+
+const root = new Node("a");
+const nodeB = new Node("b");
+const nodeC = new Node("c");
+const nodeD = new Node("d");
+const nodeE = new Node("e");
+const nodeF = new Node("f");
+
+root.left = nodeB;
+root.right = nodeC;
+nodeB.left = nodeD;
+nodeB.right = nodeE;
+nodeC.right = nodeF;
+
+const res = depthFirstTraversal(root);
+console.log(res);

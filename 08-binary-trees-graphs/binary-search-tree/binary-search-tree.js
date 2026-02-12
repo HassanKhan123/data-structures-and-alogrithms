5dde935e6fe4dd34e5a90ec886705d633905fd50
HassanKhan123 <hk2602809@gmail.com>
2026-02-12 10:02:47 +0100

    added: binary search tree

---
+++ 08-binary-trees-graphs/binary-search-tree/binary-search-tree.js
@@ -0,0 +1,55 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          }
+          current = current.left;
+        } else {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
+
+  printTree() {
+    const printNode = (node) => {
+      if (node === null) return;
+      printNode(node.left);
+      console.log(node.value);
+      printNode(node.right);
+    };
+    printNode(this.root);
+  }
+}
+
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(2);
+
+bst.printTree();

b9e7f34b41d5529f54d60de9360254e1d1c018da
HassanKhan123 <hk260280@gmail.com>
2026-02-13 20:19:43 +0100

    solved: selection sort

---
+++ 09-sorting-algorithms/selection-sort.js
@@ -0,0 +1,20 @@
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+};
+
+const res = selectionSort([4, 3, 2, 10, 12, 1, 5, 6]);
+console.log(res);

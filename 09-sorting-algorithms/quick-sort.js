29e18bf7d36bc4f80c98f305c1ca17a1a929611c
HassanKhan123 <hk260280@gmail.com>
2026-02-16 12:31:04 +0100

    solved: quick sort

---
+++ 09-sorting-algorithms/quick-sort.js
@@ -0,0 +1,21 @@
+const quickSort = (arr) => {
+  if (arr.length <= 1) return arr;
+
+  const pivot = arr[arr.length - 1];
+
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+};
+
+const res = quickSort([4, 3, 2, 10, 12, 1, 5, 6]);
+console.log(res);
